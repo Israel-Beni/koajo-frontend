7f8742db1180dec230fad8e6eaec1ef5890e69d0
Israel-Beni <bendegbe72@gmail.com>
2025-07-23 00:42:01 +0200

    feat: testing next-video integration and configs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import Video from 'next-video';
+import getStarted from '/public/media/videos/get-started.mp4';
+import TheSongVideo from "/public/media/videos/VID-20250322-WA0007.mp4"
 
 export default function Home() {
   return (
-    <></>
+    <>
+     <Video src={getStarted} />
+     <Video src={TheSongVideo} />
+     </>
+
   );
 }
