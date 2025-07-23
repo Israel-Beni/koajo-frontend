2fab8671dccb916a0aa9f27e5374fbaf2ca5faf8
Israel-Beni <bendegbe72@gmail.com>
2025-07-24 00:07:31 +0200

    refactor: rename About component to AboutPage for clarity

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Team from "@/component/team";
 import Value from "@/component/value";
 import Hero from "@/component/hero-about";
 
-export default function About() {
+export default function AboutPage() {
   return (
     <>
       <Hero />
