0f39ac44e5741c4cb15f9076caf3f82d3d1a87c8
Israel-Beni <bendegbe72@gmail.com>
2025-07-23 19:13:22 +0200

    feat: integrate Hero and Pricing components into Home page, commenting out video sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import Video from 'next-video';
 import getStarted from '/public/media/videos/get-started.mp4';
 import TheSongVideo from "/public/media/videos/VID-20250322-WA0007.mp4"
+import Pricing from "@/component/pricing";
+import Hero from "@/component/hero-home";
 
 export default function Home() {
   return (
     <>
-     <Video src={getStarted} />
-     <Video src={TheSongVideo} />
+     {/* <Video src={getStarted} />
+     <Video src={TheSongVideo} /> */}
+     <Hero />
+     <Pricing />
      </>
 
   );
