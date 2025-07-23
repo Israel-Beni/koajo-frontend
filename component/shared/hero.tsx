b6a9f2855170c1363743cf1865e089461ceb0d84
Israel-Beni <bendegbe72@gmail.com>
2025-07-24 00:06:29 +0200

    feat: add new Hero component for displaying title and description with background image

---
+++ component/shared/hero.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+
+export default function Hero({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return (
+    <section className="w-full bg-gray pt-2.5">
+      <div className="page_container">
+        <div className="min-h-[calc(384rem/16)] py-15 flex  justify-center items-center rounded-[0.875rem] bg-white">
+          {/* Background */}
+          <div className="hidden md:block size-full absolute inset-0 z-0 rounded-[inherit]">
+            <Image
+              src="/media/images/background-grid-light.svg"
+              alt="Hero Background"
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          {/* Content */}
+          <div className="inner_container relative z-10 flex flex-col ">
+            <h2
+              className="text-xl font-semibold text-text-500 mb-4"
+              dangerouslySetInnerHTML={{ __html: title }}
+            />
+            <div
+              className="text-base text-text-400 mb-6"
+              dangerouslySetInnerHTML={{ __html: description }}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
