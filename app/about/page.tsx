77be50cfb1e04dc45d227623281216e72001f073
Israel-Beni <bendegbe72@gmail.com>
2025-07-23 19:13:14 +0200

    feat: create About page to integrate Hero, Value, Benefits, and Team components

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import Benefits from "@/component/benefits";
+import Team from "@/component/team";
+import Value from "@/component/value";
+import Hero from "@/component/hero-about";
+
+export default function About() {
+  return (
+    <>
+      <Hero />
+      <Value />
+      <Benefits />
+      <Team />
+    </>
+  );
+}
