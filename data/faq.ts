73991a66abbeb8540ebf27cf6909fff3b16c6949
Israel-Beni <bendegbe72@gmail.com>
2025-07-23 22:57:13 +0200

    feat: add FAQ data structure with categories and topics for user guidance

---
+++ data/faq.ts
@@ -0,0 +1,266 @@
+export const faqsData = [
+  {
+    category: "Getting Started",
+    icon: "/media/icons/calendar.svg",
+    slug: "getting-started",
+    description: "Lorem ipsum dolor sit amet nulla vitae imperdiet.",
+    topics: [
+      {
+        title: "How to create a free Koajo account?",
+        slug: "how-to-create-a-free-koajo-account",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        quote:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        body: [
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Customer Service",
+    icon: "/media/icons/message.svg",
+    slug: "customer-service",
+    description: "Lorem ipsum dolor sit amet nulla vitae imperdiet.",
+    topics: [
+      {
+        title: "How to create a free Koajo account?",
+        slug: "how-to-create-a-free-koajo-account",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        quote:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        body: [
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Account Security",
+    icon: "/media/icons/security.svg",
+    slug: "account-security",
+    description: "Lorem ipsum dolor sit amet nulla vitae imperdiet.",
+    topics: [
+      {
+        title: "How to create a free Koajo account?",
+        slug: "how-to-create-a-free-koajo-account",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        quote:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        body: [
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Advanced settings",
+    icon: "/media/icons/settings.svg",
+    slug: "advanced-settings",
+    description: "Lorem ipsum dolor sit amet nulla vitae imperdiet.",
+    topics: [
+      {
+        title: "How to create a free Koajo account?",
+        slug: "how-to-create-a-free-koajo-account",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        quote:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        body: [
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Troubleshooting",
+    icon: "/media/icons/error.svg",
+    slug: "troubleshooting",
+    description: "Lorem ipsum dolor sit amet nulla vitae imperdiet.",
+    topics: [
+      {
+        title: "How to create a free Koajo account?",
+        slug: "how-to-create-a-free-koajo-account",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        quote:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        body: [
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Build Your Network",
+    icon: "/media/icons/build.svg",
+    slug: "build-your-network",
+    description: "Lorem ipsum dolor sit amet nulla vitae imperdiet.",
+    topics: [
+      {
+        title: "How to create a free Koajo account?",
+        slug: "how-to-create-a-free-koajo-account",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        quote:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        body: [
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Documentation",
+    icon: "/media/icons/document.svg",
+    slug: "documentation",
+    description: "Lorem ipsum dolor sit amet nulla vitae imperdiet.",
+    topics: [
+      {
+        title: "How to create a free Koajo account?",
+        slug: "how-to-create-a-free-koajo-account",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        quote:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        body: [
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: "Real-Life Lessons",
+    icon: "/media/icons/lesson.svg",
+    slug: "real-life-lessons",
+    description: "Lorem ipsum dolor sit amet nulla vitae imperdiet.",
+    topics: [
+      {
+        title: "How to create a free Koajo account?",
+        slug: "how-to-create-a-free-koajo-account",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        quote:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+        body: [
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+          {
+            title: "How to create a free Koajo account?",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquet dui ipsum vitae rhoncus dolor maximus quis. Sed a eleifend ligula. Sed omare eleifend augue.",
+          },
+        ],
+      },
+    ],
+  },
+];
