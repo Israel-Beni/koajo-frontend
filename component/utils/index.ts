d51e617630c5e7d5f8f260dea2b90e26921e0984
Israel-Beni <bendegbe72@gmail.com>
2025-07-23 15:15:25 +0200

    feat: add MyCldImage component for Cloudinary image support and update utils index

--- component/utils/index.ts
+++ component/utils/index.ts
@@ -2,3 +2,4 @@ export { default as MyImage } from './my-image';
 export { Button } from './button';
 export type { MyImageProps, ResponsiveImageConfig } from './my-image';
 export { default as PlanCard } from './card-plan'; 
+export { default as MyCldImage } from './my-cld-image';
