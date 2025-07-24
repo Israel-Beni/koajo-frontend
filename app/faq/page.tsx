0cea186c08ea9e1e2cd1ea892fc5164ebf24fcc5
Israel-Beni <bendegbe72@gmail.com>
2025-07-24 23:20:44 +0200

    refactor: eradicated faq v1

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,17 +1,5 @@
-import React from "react";
-import CardFaq from "@/component/utils/card-faq-topic";
-import { Button } from "@/component/utils";
-import Hero from "@/component/shared/hero";
-import FaqCategories from "@/component/faq-categories-listings";
+import ComingSoon from "@/component/shared/coming-soon";
 
 export default function FaqPage() {
-  return (
-    <>
-      <Hero
-        title="IT department manager"
-        description="Lorem ipsum doloramet consectetur etiam urna suspendisse risus molestiefames elementum porta lamcorper semid senectus."
-      />
-      <FaqCategories />
-    </>
-  );
+  return <ComingSoon />;
 }
