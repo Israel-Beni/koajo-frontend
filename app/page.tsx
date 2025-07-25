a2ad82fecd5f9d922535914b589581744d086058
Israel-Beni <bendegbe72@gmail.com>
2025-07-25 02:09:33 +0200

    feat: integrate Solutions component into HomePage to enhance user experience and service visibility

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Pricing from "@/component/pricing";
 import Hero from "@/component/hero-home";
 import Benefits from "@/component/benefits";
 import Steps from "@/component/steps";
+import Solutions from "@/component/solutions";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <Solutions />
       <Steps />
       <Benefits />
       <Pricing />
