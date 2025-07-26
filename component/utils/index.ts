57307f9a905330e86e8a1fd7d79b1f2dfbb7bb0d
Israel-Beni <bendegbe72@gmail.com>
2025-07-26 02:23:08 +0200

    feat: update utils index to include MyResponsivePie and Donot components

--- component/utils/index.ts
+++ component/utils/index.ts
@@ -3,3 +3,5 @@ export { Button } from './button';
 export type { MyImageProps, ResponsiveImageConfig } from './my-image';
 export { default as PlanCard } from './card-plan'; 
 export { default as MyCldImage } from './my-cld-image';
+export { default as MyResponsivePie } from './my-responsive-pie';
+export { default as Donot } from "./donot"
