ed71f4f57d2fc0bcaec6335c1f923784207c054c
Israel-Beni <bendegbe72@gmail.com>
2025-07-26 18:18:46 +0200

    feat: add Hero component to FAQ page with title and description for enhanced user engagement

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,5 +1,13 @@
+import Hero from "@/component/shared/hero";
 import ComingSoon from "@/component/shared/coming-soon";
 
 export default function FaqPage() {
-  return <ComingSoon />;
+  return (
+    <>
+      <Hero
+        title="Frequently Asked <sapn class='text-highlight'>Questions</sapn>"
+        description="Answers to all your questions regarding our platform, how it serves you and ensures integrity for all users."
+      />
+    </>
+  );
 }
