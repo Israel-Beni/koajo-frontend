1495b2051b9da8c7fecfdf9dfc9b9fb703ac7935
Israel-Beni <bendegbe72@gmail.com>
2025-07-26 04:24:14 +0200

    feat: add gray and teal color variables to globals.css for enhanced styling options

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/component/hero-home";
 import Benefits from "@/component/benefits";
 import Steps from "@/component/steps";
 import Solutions from "@/component/solutions";
-
+import Features from "@/component/features";
 
 export default function HomePage() {
   return (
@@ -12,6 +12,7 @@ export default function HomePage() {
       <Solutions />
       <Steps />
       <Benefits />
+      <Features />
       <Pricing />
     </>
   );
