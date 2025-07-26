d4bc12e211e066479ecdfd94d526a32ed2644ff1
Israel-Beni <bendegbe72@gmail.com>
2025-07-26 05:44:32 +0200

    feat: add BlogPage and LegalsPage components with ComingSoon placeholder

---
+++ app/blog/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from "@/component/shared/coming-soon";
+
+export default function BlogPage() {
+  return <ComingSoon />;
+}
---
+++ app/legals/[slug]/page.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from "@/component/shared/coming-soon";
+
+export default function LegalsPage() {
+  return <ComingSoon />;
+}
