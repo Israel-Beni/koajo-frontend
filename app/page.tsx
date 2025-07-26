0e2003e400c52a38289c72822b4cf85c539619da
Israel-Beni <bendegbe72@gmail.com>
2025-07-26 05:40:03 +0200

    feat: integrate Marques component into HomePage for enhanced visual appeal

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import Benefits from "@/component/benefits";
 import Steps from "@/component/steps";
 import Solutions from "@/component/solutions";
 import Features from "@/component/features";
+import Marques from "@/component/marques";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <Solutions />
+      <Marques />
       <Steps />
       <Benefits />
       <Features />
