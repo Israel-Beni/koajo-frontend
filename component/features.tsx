7171c5f02edf2e824692d5226955244477b1d650
Israel-Beni <bendegbe72@gmail.com>
2025-08-02 00:23:39 +0200

    refactor(features): simplify feature rendering logic and enhance responsive layout for better display on different screen sizes

--- component/features.tsx
+++ component/features.tsx
@@ -1,6 +1,25 @@
 import { MyImage, Donot } from "./utils";
+import cn from "clsx"
 
 export default function Features() {
+  const renderFeature = (feature: Feature, idx?: number, className = "") => {
+    return (
+      <div
+        key={idx}
+        className={cn("px-5 py-10 bg-teal-800 rounded-[calc(22.5rem/16)] border-2 border-teal-800", className)}
+      >
+        {feature.Visual && (
+          <div className="size-[calc(221rem/16)] mb-6 mx-auto">
+            <feature.Visual />
+          </div>
+        )}
+        <h3 className="text-2lg font-semibold text-white mb-4">
+          {feature.title}
+        </h3>
+        <p className="text-sm text-text-200">{feature.description}</p>
+      </div>
+    );
+  };
   return (
     <section className="w-full bg-gray">
       <div className="page_container py-15">
@@ -18,28 +37,30 @@ export default function Features() {
         </div>
 
         {/* features */}
-        <div className="flex flex-col gap-5">
-          {features.map((feature, idx) => (
-            <div
-              key={idx}
-              className="px-5 py-10 bg-teal-800 rounded-[calc(22.5rem/16)] border-2 border-teal-800"
-            >
-              {feature.Visual && (
-                <div className="size-[calc(221rem/16)] mb-6 mx-auto">
-                  <feature.Visual />
-                </div>
-              )}
-              <h3 className="text-2lg font-semibold text-white mb-4">
-                {feature.title}
-              </h3>
-              <p className="text-sm text-text-200">{feature.description}</p>
-            </div>
-          ))}
+        <div className="hidden lg:grid grid-cols-3 gap-5">
+          {renderFeature(features[0])}
+          <div className="flex flex-col gap-5">
+            {features
+              .slice(1, 3)
+              .map((feature, idx) => renderFeature(feature, idx, "h-full"))}
+          </div>
+          {renderFeature(features[3])}
+        </div>
+
+        <div className="flex lg:hidden flex-col gap-5">
+          {features.map((feature, idx) => renderFeature(feature, idx))}
         </div>
       </div>
     </section>
   );
 }
+
+interface Feature {
+  title: string;
+  description: string;
+  Visual?: () => React.ReactNode;
+}
+
 export const features = [
   {
     title: "12 or 24 Week Structured Pod Cycle",
@@ -76,6 +97,15 @@ export const features = [
     title: "Automated Contributions and Payouts",
     description:
       "Enjoy a smooth, hands-free experience with automated transactions. Monitor your savings, contributions, and upcoming payouts anytime through your dashboard.",
-    Visual: () => <MyImage src="payouts_x8jwu8" alt="Payment flow" width={221} height={221} className="max-h-[calc(221rem/16)] size-auto" isCloudinary />,
+    Visual: () => (
+      <MyImage
+        src="payouts_x8jwu8"
+        alt="Payment flow"
+        width={221}
+        height={221}
+        className="max-h-[calc(221rem/16)] size-auto"
+        isCloudinary
+      />
+    ),
   },
 ];
