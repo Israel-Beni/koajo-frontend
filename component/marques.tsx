f6c5e39cb22098532aa5d0d840505fc16d05b116
Israel-Beni <bendegbe72@gmail.com>
2025-08-01 22:10:38 +0200

    style(marques): enhance layout and responsiveness of Marques component with updated height and typography adjustments

--- component/marques.tsx
+++ component/marques.tsx
@@ -6,7 +6,7 @@ export default function Marques() {
       <div className="page_container flex flex-col gap-4.5 px-0">
         <Marquee
           speed={70}
-          className="relative z-10 flex items-center gap-2.5 -rotate-2 h-12.5 bg-black py-1.5 lg:py-2.5 text-xl font-semibold text-white"
+          className="relative z-10 flex items-center gap-2.5 -rotate-2 h-12.5 md:h-14 lg:h-17 bg-black py-1.5 lg:py-2.5 text-xl md:text-2xl lg:text-3xl font-semibold text-white"
         >
           Ajo Esusu Tandas Kye Susu Tandas Njangi Mikado Poro Lughu Vumi
           Harambee
@@ -27,7 +27,7 @@ export default function Marques() {
 
 function Badge({ text }: { text: string }) {
   return (
-    <div className="flex items-center py-2.5 px-5.5 text-lg font-medium text-gray-600 rounded-full border border-text-300 mr-1.5">
+    <div className="flex items-center py-2.5 md:py-3.5 lg:py-4 px-5.5 md:px-7.5 lg:px-10 text-lg md:text-xl lg:text-2xl font-medium text-gray-600 rounded-full border border-text-300 mr-1.5">
       {text}
     </div>
   );
