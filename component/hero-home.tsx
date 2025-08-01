118fa418f8b1d1fdcb24c960480b8ba979fc3c3e
Israel-Beni <bendegbe72@gmail.com>
2025-08-01 21:42:54 +0200

    style(hero-home): improve layout and responsiveness of Hero Home component with updated structure and spacing adjustments

--- component/hero-home.tsx
+++ component/hero-home.tsx
@@ -17,47 +17,59 @@ export default function Hero() {
             />
           </div>
 
-          {/* Tagline */}
-          <div className="flex items-center justify-center gap-2 mb-4 lg:mb-5 relative z-10">
-            <span className="text-xs md:text-sm lg:text-md text-white/90">1200+ 5 Stars Reviews</span>
-            <MyCldImage
-              src="users-avatars_bwcuqz"
-              alt="Users avatars"
-              width={100}
-              height={29.4}
-              crop="fit"
-              gravity="center"
-              className="w-auto  h-7.5 object-contain"
-              quality={100}
+          <div className="flex flex-col items-center lg:max-w-[calc(1078rem/16)]">
+            {/* Tagline */}
+            <div className="flex items-center justify-center gap-2 mb-4 lg:mb-5 relative z-10">
+              <span className="text-xs md:text-sm lg:text-md text-white/90">
+                1200+ 5 Stars Reviews
+              </span>
+              <MyCldImage
+                src="users-avatars_bwcuqz"
+                alt="Users avatars"
+                width={100}
+                height={29.4}
+                crop="fit"
+                gravity="center"
+                className="w-auto  h-7.5 object-contain"
+                quality={100}
+              />
+            </div>
+
+            {/* Headline */}
+            <h1 className="text-xl md:text-3xl lg:text-5xl font-semibold text-white text-center  mb-5 lg:mb-6 relative z-10">
+              Savings. Financial Security.{" "}
+              <span className="text-highlight">Financial</span> Freedom
+            </h1>
+            {/* Subheading */}
+            <p className="text-sm md:text-md lg:text-lg text-white/90 text-center mb-6 lg:mb-8 relative z-10">
+              We are on a mission to redefine savings and wealth-building for
+              millions.
+            </p>
+
+            <Button
+              text="Join the Community"
+              className="mb-9 font-semibold relative z-10"
             />
           </div>
-
-          {/* Headline */}
-          <h1 className="text-xl md:text-3xl lg:text-5xl font-semibold text-white text-center  mb-5 lg:mb-6 relative z-10">
-            Savings. Financial Security.{" "}
-            <span className="text-highlight">Financial</span> Freedom
-          </h1>
-          {/* Subheading */}
-          <p className="text-sm md:text-md lg:text-lg text-white/90 text-center mb-6 lg:mb-8 relative z-10">
-            We are on a mission to redefine savings and wealth-building for
-            millions.
-          </p>
-
-          <Button text="Join the Community" className="mb-9 font-semibold relative z-10" />
         </div>
 
         {/* Stats Card */}
-        <div className="inner_container">
-        <MyImage
-          src="hero-home_zafbgr"
-          alt="Users avatars"
-          width={293}
-          height={347}
-          className="rounded-lg size-auto relative -top-[calc(124rem/16)] mx-auto z-10"
-          isCloudinary
-          quality={100}
-          priority
-        />
+        <div className="inner_container lg:max-w-[calc(1078rem/16)] mx-auto">
+          <MyImage
+            src="hero-home_zafbgr"
+            alt="Users avatars"
+            width={293}
+            height={347}
+            desktop={{
+              width: 1078,
+              height: 571,
+              src: "hero-home-desktop_iy9o4j",
+            }}
+            className="rounded-lg size-auto relative -top-[calc(124rem/16)] mx-auto z-10"
+            isCloudinary
+            quality={100}
+            priority
+          />
         </div>
       </div>
     </section>
