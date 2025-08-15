0cdf7bcfbcdf4949aca839be505080f81f315a92
Israel-Beni <bendegbe72@gmail.com>
2025-08-15 22:58:08 +0200

    feat(page): add FaqPage component with Hero and FaqListings sections

---
+++ app/(main)/faq/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/shared/hero";
+import FaqListings from "@/components/faq-listings";
+
+export default function FaqPage() {
+  return (
+    <>
+      <Hero
+        title="Frequently Asked <sapn class='text-highlight'>Questions</sapn>"
+        description="Answers to all your questions regarding our platform, how it serves you and ensures integrity for all users."
+      />
+      <FaqListings />
+    </>
+  );
+}
