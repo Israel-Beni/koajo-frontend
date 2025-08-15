18846b4c85cd87c385ba685deaad8223fab01465
Israel-Beni <bendegbe72@gmail.com>
2025-08-15 23:05:26 +0200

    feat(hooks): introduce useIsServerSide hook to determine server-side rendering status

---
+++ lib/hooks/useIsServerSide.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export const useIsServerSide = () => {
+    const [isServerSide, setIsServerSide] = useState(true);
+
+    useEffect(() => {
+        setIsServerSide(false);
+    }, [setIsServerSide]);
+
+    return isServerSide;
+};
