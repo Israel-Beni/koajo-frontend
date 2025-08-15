a7a902f062e5798f22af48f787a615c0fa210fd7
Israel-Beni <bendegbe72@gmail.com>
2025-08-15 23:01:41 +0200

    chore(utils): remove utility exports and associated code

--- component/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as MyImage } from './my-image';
-export { Button } from './button';
-export type { MyImageProps, ResponsiveImageConfig } from './my-image';
-export { default as PlanCard } from './card-plan'; 
-export { default as MyCldImage } from './my-cld-image';
-export { default as MyResponsivePie } from './my-responsive-pie';
-export { default as Donot } from "./donot"
-export { default as Modal } from "./modal"
