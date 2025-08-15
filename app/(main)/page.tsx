9cfa1ba73af458a11bdd849b854b8fc524142de9
Israel-Beni <bendegbe72@gmail.com>
2025-08-15 22:57:33 +0200

    feat(page): add HomePage component with imports for various sections and commented-out structure

---
+++ app/(main)/page.tsx
@@ -0,0 +1,26 @@
+import Pricing from "@/components/pricing";
+import Hero from "@/components/hero-home";
+import Benefits from "@/components/benefits";
+import Steps from "@/components/steps";
+import Solutions from "@/components/solutions";
+import Features from "@/components/features";
+import Marques from "@/components/marques";
+import Comparison from "@/components/comparison";
+import { Modal } from "@/components/utils";
+
+export default function HomePage() {
+  return (
+    <>
+      {/* <Hero />
+      <Solutions />
+      <Marques />
+      <Steps />
+      <Benefits />
+      <Features />
+      <Comparison />
+      <Pricing /> */}
+
+     {/* <Modal visible={true} >Hello world!</Modal>; */}
+    </>
+  );
+}
