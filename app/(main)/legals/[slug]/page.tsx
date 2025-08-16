bda8a3e84c781c055efae59a2e14750fcf612a41
Israel-Beni <bendegbe72@gmail.com>
2025-08-16 23:49:51 +0200

    refactor(legals): streamline HTML structure and improve class application for better layout consistency

--- app/(main)/legals/[slug]/page.tsx
+++ app/(main)/legals/[slug]/page.tsx
@@ -1,10 +1,8 @@
 import { legalPages } from "@/data/legal-pages";
-import { HTMLTag } from "@/types";
 import { notFound } from "next/navigation";
 
 const legalPageSlugs = legalPages.map((page) => page.slug);
 
-// statically generate list of legal pages
 export const generateStaticParams = async () => {
   return legalPageSlugs.map((slug) => ({ slug }));
 };
@@ -25,32 +23,31 @@ export default function LegalsPage({ params }: { params: { slug: string } }) {
       .addClassToHtmlTag("h3", "text-sm md:text-base lg:text-md font-semibold text-text-500")
       .addClassToHtmlTag("strong", "font-semibold text-text-500")
       .addClassToHtmlTag("a", "text-primary")
-      .bulkAddClassToHtmlTag(["ul" , "ol"], "list-disc list-inside marker:text-text-500")
+      .bulkAddClassToHtmlTag(["ul", "ol"], "list-disc list-inside");
     
-    return {body: processedBody, ...rest}
-  })
+    return {body: processedBody, ...rest};
+  });
 
   return (
-    
-    <section className="w-full py-10 md:py-15 lg:py-20">
+    <div className="w-full py-10 md:py-15 lg:py-20">
       <div className="page_container">
         <div className="max-w-[calc(1006rem/16)] mx-auto flex flex-col gap-5 md:gap-7 lg:gap-10">
           {/* Heading */}
-          <div className="z-10 flex flex-col gap-5 md:gap-6">
+          <section className="z-10 flex flex-col gap-5 md:gap-6">
             <h1
-              className="text-2xl md:text-3xl lg:text-5xl font-semibold text-text-600 text-center "
+              className="text-2xl md:text-3xl lg:text-5xl font-semibold text-text-600 text-center"
               dangerouslySetInnerHTML={{ __html: title }}
             />
             <p className="text-base md:text-md text-text-400">
               {description}
             </p>
-          </div>
+          </section>
 
           {/* Content */}
-          <div className="flex flex-col gap-5 md:gap-7 lg:gap-10">
+          <section className="flex flex-col gap-5 md:gap-7 lg:gap-10">
             {processedContent?.length && processedContent.map((item, index) => (
               <div key={index} className="flex flex-col gap-4">
-                {'title' in item && item.title && (
+                {item.title && (
                   <h2 className="text-md md:text-lg lg:text-2lg font-bold text-gray-900">
                     {item.title}
                   </h2>
@@ -61,9 +58,9 @@ export default function LegalsPage({ params }: { params: { slug: string } }) {
                 />
               </div>
             ))}
-          </div>
+          </section>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
