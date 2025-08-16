f79057ff296bd1c2f10b8fcee183a7cd1277bd4c
Israel-Beni <bendegbe72@gmail.com>
2025-08-16 23:25:57 +0200

    feat(types): add type definitions for number ranges and HTML tags

---
+++ types/index.ts
@@ -0,0 +1,78 @@
+/**
+ * Creates a union type of numbers from 0 to N (inclusive)
+ * This is a generic and declarative approach to create bounded number types
+ */
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>;
+
+/**
+ * Creates a range type from F to T (inclusive)
+ */
+export type Range<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>> | T;
+
+
+export type HTMLTag =
+  | "strong"
+  | "i"
+  | "u"
+  | "s"
+  | "code"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "p"
+  | "ul"
+  | "ol"
+  | "li"
+  | "blockquote"
+  | "figure"
+  | "figcaption"
+  | "img"
+  | "a"
+  | "table"
+  | "thead"
+  | "tbody"
+  | "tr"
+  | "td"
+  | "th"
+  | "caption"
+  | "hr"
+  | "br"
+  | "video"
+  | "audio"
+  | "iframe"
+  | "canvas"
+  | "svg"
+  | "path"
+  | "rect"
+  | "circle"
+  | "line"
+  | "polyline"
+  | "polygon"
+  | "text"
+  | "tspan"
+  | "g"
+  | "use"
+  | "defs"
+  | "linearGradient"
+  | "radialGradient"
+  | "stop"
+  | "defs"
+  | "clipPath"
+  | "mask"
+  | "pattern"
+  | "symbol"
+  | "use"
+  | "title"
+  | "desc"
+  | "metadata"
+  | "script"
+  | "style"
+  | "noscript"
+  | "template"
+  | "slot"
+  | "slot";
