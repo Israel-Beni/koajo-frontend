f77d7c1b7feb927ad9c3dcac1fa86b19fd328837
Israel-Beni <bendegbe72@gmail.com>
2025-08-19 12:45:41 +0200

    refactor(Layout): clean up component structure and re-enable page scroll effect on pathname change

--- components2/usefull/Layout/index.tsx
+++ components2/usefull/Layout/index.tsx
@@ -35,40 +35,40 @@ const Layout = ({
 }: LayoutProps) => {
   const pathname = usePathname();
 
-  // useEffect(() => {
-  //     // clearQueueScrollLocks();
-  //     enablePageScroll();
-  // }, [pathname]);
+  useEffect(() => {
+      // clearQueueScrollLocks();
+      enablePageScroll();
+  }, [pathname]);
 
   return (
-    <>
-      <div className={styles.layout}>
-        <div className={cn(styles.head, classHead)}>
-          <div className={cn("page_dashboard_container", styles.container)}>
-            <Header />
-            <div className={styles.title}>{title}</div>
-            <div className={styles.breadcrumbs}>
-              {breadcrumbs.map((item, index) =>
-                item.url ? (
-                  <Link className={styles.link} href={item.url} key={index}>
-                    {item.title}
-                  </Link>
-                ) : (
-                  <div className={styles.text} key={index}>
-                    <Icon name="arrow-next" size="12" />
-                    {item.title}
-                  </div>
-                )
-              )}
-            </div>
-            {head}
+    <div className={styles.layout}>
+      <div className={cn(styles.head, classHead)}>
+        <div className={cn("page_dashboard_container", styles.container)}>
+          <Header />
+          <div className={styles.title}>{title}</div>
+          <div className={styles.breadcrumbs}>
+            {breadcrumbs.map((item, index) =>
+              item.url ? (
+                <Link className={styles.link} href={item.url} key={index}>
+                  {item.title}
+                </Link>
+              ) : (
+                <div className={styles.text} key={index}>
+                  <Icon name="arrow-next" size="12" />
+                  {item.title}
+                </div>
+              )
+            )}
           </div>
-        </div>
-        <div className={cn(styles.body, classBody)}>
-          <div className={cn("page_dashboard_container", styles.container)}>{children}</div>
+          {head}
         </div>
       </div>
-    </>
+      <div className={cn(styles.body, classBody)}>
+        <div className={cn("page_dashboard_container", styles.container)}>
+          {children}
+        </div>
+      </div>
+    </div>
   );
 };
 
